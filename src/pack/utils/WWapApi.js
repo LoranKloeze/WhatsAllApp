5737901b957b14b5a14a4c0ac28987b868a00fe4
Roel Kramer <contact@roelkramer.nl>
2019-03-02 13:13:41 +0100

    Fix bug 64 which was caused by a WhatsApp web code change
    * Find WLAPStore and in WLAPWAPStore in all app*js scripts now
    * Fix regex for WLAPStore

--- src/pack/utils/WWapApi.js
+++ src/pack/utils/WWapApi.js
@@ -4,6 +4,30 @@ if (process.env.NODE_ENV === 'development') {
     window.WAA_Api = Api
 }
 
+function findWLAPWAPStore(js_src, url) {
+    const regExDynNameStore = /Wap:[a-z]\('"(\w+)"'\)/
+    const res = regExDynNameStore.exec(js_src)
+    if(!res) {
+        return
+    }
+
+    const funcName = res[1]
+    console.log(`findWLAPWAPStore: ${funcName}`)
+    window.webpackJsonp([], {[funcName]: (x, y, z) => {Api.WLAPWAPStore = z('"' + funcName + '"')}}, funcName)
+}
+
+function findWLAPStore(js_src, url) {
+    const regExDynNameStore = /'"(\w+)"':function\(e,t,i\)\{\"use strict\";e\.exports=\{AllStarredMsgs:/
+    const res = regExDynNameStore.exec(js_src)
+    if(!res) {
+        return
+    }
+
+    const funcName = res[1]
+    console.log(`findWLAPStore: ${funcName}`)
+    window.webpackJsonp([], {[funcName]: (x, y, z) => Api.WLAPStore = z('"' + funcName + '"')}, funcName)
+}
+
 export const initApi = () => {
     return new Promise((resolve, reject) => {
         /*
@@ -14,72 +38,50 @@ export const initApi = () => {
         * */
 
         const scripts = document.getElementsByTagName('script')
-        const regexApp = /\/app\..+.js/
-        const regexApp2 = /\/app2\..+.js/
-        let appScriptUrl, app2ScriptUrl
+        const regexAppScript = /\/app\d?\..+.js/
+        let appScriptUrls = [];
 
         // Derive script urls
         for (let i = 0; i < scripts.length; i++) {
             const src = scripts[i].src
-            if (regexApp.exec(src) != null) {
-                appScriptUrl = src
-            }
-
-            if (regexApp2.exec(src) != null) {
-                app2ScriptUrl = src
+            if (regexAppScript.exec(src) != null) {
+                console.log("General: "+ src)
+                appScriptUrls.push(src)
             }
         }
 
-        // Download scripts, regex them and assign store
-        fetch(appScriptUrl).then(e => {
-            const reader = e.body.getReader()
-            let js_src = ""
-
-            return reader.read().then(function readMore({done, value}) {
-                const td = new TextDecoder("utf-8")
-                const str_value = td.decode(value)
-                if (done) {
-                    js_src += str_value
-                    const regExDynNameStore = /Wap:[a-z]\('"(\w+)"'\)/
-                    const res = regExDynNameStore.exec(js_src)
-                    const funcName = res[1]
-                    window.webpackJsonp([], {[funcName]: (x, y, z) => {Api.WLAPWAPStore = z('"' + funcName + '"')}}, funcName)
-                    return
-                }
-
-                js_src += str_value
-                return reader.read().then(readMore)
-
-            })
-
-        }).then(() => {
-            fetch(app2ScriptUrl).then(e => {
-                const reader = e.body.getReader()
-                let js_src = ""
-
-                return reader.read().then(function readMore({done, value}) {
+        let promises = Promise.all(appScriptUrls.map((url) => fetch(url)))
+        promises.then((results) => {
+            results.forEach((e) => {
+                console.log(`Reading ${e.url}`);
+                let reader = e.body.getReader();
+                let js_src = "";
+                reader.read().then(function readMore({done, value}) {
+                    // join sources
                     const td = new TextDecoder("utf-8")
                     const str_value = td.decode(value)
                     if (done) {
                         js_src += str_value
-                        const regExDynNameStore = /'"(\w+)"':function\(e,t,a\)\{\"use strict\";e\.exports=\{AllStarredMsgs:/
-                        const res = regExDynNameStore.exec(js_src)
-                        const funcName = res[1]
-
-                        window.webpackJsonp([], {[funcName]: (x, y, z) => Api.WLAPStore = z('"' + funcName + '"')}, funcName)
-                        resolve()
+                        if(!Api.WLAPWAPStore) {
+                            findWLAPWAPStore(js_src, e.url)
+                        }
+                        if(!Api.WLAPStore) {
+                            findWLAPStore(js_src, e.url)
+                        }
                         return
                     }
 
-                    js_src += str_value
+                    js_src += str_value;
                     return reader.read().then(readMore)
-
+                }).then(() => {
+                    if (Api && typeof Api.WLAPStore === "object" && typeof Api.WLAPWAPStore === "object") {
+                        resolve()
+                    }
                 })
-
-            })
-        })
-
-
+            });
+        }).catch((error) => {
+            console.error(error);
+        });
     })
 }
 
@@ -89,6 +91,4 @@ export const getApi = () => {
     } else {
         console.error('WWapApi is not initialized, call initApi() first')
     }
-
-
 }
